0baf081c56be972313d049537c39dfc7b60fc99a
Arthur Dhuy <a.dhuy@orange.fr>
2019-02-09 13:07:28 +0100

    Added validations on model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps
+  include ActiveModel::Validations
+
   field :title, type: String
   field :body, type: String
   field :url, type: String
+
+  validates_presence_of :title
+  validates_length_of :body, maximum: 30, message: 'maximum of 30 characters'
+  validates_format_of :url, with: /\A(^www+.+\w+.+(com|fr|io|org)$)\z/
 end
